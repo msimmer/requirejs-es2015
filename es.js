58ffd2485753ccb32f7219689f1124c952b1802a
Maxwell Simmer <maxwell.simmer@gmail.com>
2017-06-27 12:33:43 +0200

    Load plugins and presets from .babelrc

--- es.js
+++ es.js
@@ -5,15 +5,26 @@ define(function () {
 
     function transpile(name, parentRequire, onLoadNative, config) {
         var babel = require.nodeRequire('babel-core');
-        var path = parentRequire.toUrl(withExtension(name, config));
+        var find = require.nodeRequire('find-babel-config');
+        var path = require.nodeRequire('path');
+
+        var fpath = parentRequire.toUrl(withExtension(name, config));
+        var babelrc = find.sync(path.dirname(fpath)).config || { presets: [], plugins: [] };
+
+        if (babelrc.presets.indexOf('es2015') < 0) {
+            babelrc.presets.push('es2015');
+        }
+        if (babelrc.plugins.indexOf('transform-es2015-modules-amd') < 0) {
+            babelrc.plugins.push('transform-es2015-modules-amd');
+        }
 
         try {
-            var result = babel.transformFileSync(path, {
+            var result = babel.transformFileSync(fpath, {
                 sourceFileName: config.baseUrl + name,
                 sourceMaps: config.babel && config.babel.sourceMaps,
                 filename: config.baseUrl + name,
-                presets: ['es2015'],
-                plugins: ['transform-es2015-modules-amd']
+                presets: babelrc.presets,
+                plugins: babelrc.plugins,
             });
 
             buildMap[name] = result.code;
